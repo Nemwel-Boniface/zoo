e30f332a979e80941894de8654f840e7137c685d
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-17 20:11:50 +0300

    Update:  - Renamed APP components to match the project  - Re orhanized the projects file structure making it more organized

--- src/ZooContainer.js
+++ src/ZooContainer.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function ZooContainer() {
   return (
     <div className="App">
       <header className="App-header">
@@ -26,4 +26,4 @@ function App() {
   );
 }
 
-export default App;
+export default ZooContainer;
