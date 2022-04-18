ada9fc828cf27c21b416d948bb4aa7edf71c7aec
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-18 15:15:11 +0300

    Update included the combine reducer

--- src/redux/ZooConfig.js
+++ src/redux/ZooConfig.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import ZooReducer from './Zoo/Zoo';
 
+const zoored = combineReducers({ animals: ZooReducer });
 const middleWares = [thunk, logger];
 
 const store = createStore(
-  ZooReducer,
+  zoored,
   applyMiddleware(...middleWares),
 );
 export default store;
