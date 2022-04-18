a14aeb5242666102e4de7662667cae6e521a9899
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-18 08:52:04 +0300

    Update renamed stylesheets file names and included stylesheets to project

--- src/ZooContainer.js
+++ src/ZooContainer.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Header from './components/header';
 import Zoo from './components/Zoo';
 
 function ZooContainer() {
   return (
     <div className="ZooContainer">
+      <Header />
       <Zoo />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ZooContainer from './ZooContainer';
+import './stylesheets/ZooContainer.css';
+import './stylesheets/Header.css';
 
 ReactDOM.render(
   <React.StrictMode>
