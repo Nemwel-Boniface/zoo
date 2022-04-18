da66dd04c97377467cf39b820e853df8005aa932
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-18 09:02:00 +0300

    Update checked and fixed linter errors

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,20 @@
-import React from "react";
+import React from 'react';
 // import logo from '../images/simba1.webp';
 
-const Header = () => {
-  return (
-    <div className="Header">
-      <div className="logo">
-        {/* <img src={logo} alt="Simba logo"/> */}
-        <h2>Lion</h2>
-      </div>
-      <div className="zooname">
-        <h2>Zoo</h2>
-      </div>
-      <div className="headerIcons">
-        <span><i class="fa fa-microphone" aria-hidden="true"></i></span>
-        <span><i class="fa fa-cog" aria-hidden="true"></i></span>
-      </div>
+const Header = () => (
+  <div className="Header">
+    <div className="logo">
+      {/* <img src={logo} alt="Simba logo"/> */}
+      <h2>Lion</h2>
     </div>
-  )
-}
+    <div className="zooname">
+      <h2>Zoo</h2>
+    </div>
+    <div className="headerIcons">
+      <span><i className="fa fa-microphone" aria-hidden="true" /></span>
+      <span><i className="fa fa-cog" aria-hidden="true" /></span>
+    </div>
+  </div>
+);
 
-export default Header
+export default Header;
