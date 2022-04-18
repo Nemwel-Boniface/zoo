8ebd9e4495789f7567a66e6a568c7879a4de05be
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-18 12:26:04 +0300

    Update checked and fixed all store linter errors

--- src/redux/ZooConfig.js
+++ src/redux/ZooConfig.js
@@ -1,12 +1,12 @@
-import { createStore,  applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import ZooReducer from "./Zoo/Zoo"
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import ZooReducer from './Zoo/Zoo';
 
-const middleWares = [thunk, logger ]
+const middleWares = [thunk, logger];
 
 const store = createStore(
   ZooReducer,
-  applyMiddleware(...middleWares)
-)
+  applyMiddleware(...middleWares),
+);
 export default store;
