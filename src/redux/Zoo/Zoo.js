4b14b9ee5d6e62143c68b4884e5d66adcb1791e1
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-18 12:06:04 +0300

    Update included:  - Set up the Fetch animals action type to fetch the random animals from the API  - Set the reducer to handle all the returned animals from the API

--- src/redux/Zoo/Zoo.js
+++ src/redux/Zoo/Zoo.js
@@ -1,22 +1,39 @@
 import { Action } from "history";
-import { GETFROMAPI } from "./actionTypes";
+import GETFROMAPI from "./actionTypes";
 
 const baseURL = 'https://zoo-animal-api.herokuapp.com/animals/rand/10';
 
 let initialState = [];
-const ZooReducer = (state = initialState, Action) => {
+const ZooReducer = (state = initialState, action) => {
   switch(Action.type) {
     case GETFROMAPI:
       return [
-
+        ...action.payLoad
       ]
     default:
       return state;
   }
 }
 
-const getAnimalsFromAPI = () => {
-
-}
+export const getAnimalsFromAPI = () => (dispatch) => fetch(baseURL)
+  .then((res) => res.json()).then((data) => {
+    const animals = data.map((animal) => ({
+      id: animal.id,
+      name: animal.name,
+      latinName: animal.latin_name,
+      animalType: animal.animal_type,
+      activeTime: animal.active_time,
+      minLength: animal.length_min,
+      maxLength: animal.length_max,
+      minWeight: animal.weight_min,
+      maxWeight: animal.weight_max,
+      lifeSpan: animal.lifespan,
+      habitats: animal.habitat,
+      diet: animal.diet,
+      locattions: animal.geo_range,
+      Image: animal.image_link,
+    }));
+    dispatch({type: GETFROMAPI, payLoad: animals});
+  }).catch(() => {});
 
 export default ZooReducer
