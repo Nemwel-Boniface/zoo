501a8c9b663f74dfabbe9f30d5f4db6587692426
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-19 17:17:57 +0300

    Update setup the details component which displays correctly

--- src/components/details.js
+++ src/components/details.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+// import Animal from "./Animal";
 
 const Details = () => {
+  const { id } = useParams();
+  const push = useNavigate();
+  const animals = useSelector((state) => state.animals);
+  const [animal, setAnimal] = useState({});
+  useEffect(() => {
+    animals.forEach((an) => {
+      console.log('loops');
+      if(parseInt(an.id, 10) === parseInt(id, 10) ) {
+        setAnimal(an);
+        console.log('matches');
+      }
+    });
+  }, []);
   return (
     <div className="Details">
-      <h1>Further details</h1>
+      <button onClick={() => push('/')}>Go back</button>
+      <h1>Further details{id}{animal.name}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
