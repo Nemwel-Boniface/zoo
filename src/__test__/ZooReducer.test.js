9c4580a3edae3ba1fb2b7d229633d085c2b4c90f
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-20 17:18:53 +0300

    Update checked whether the number of items returned is an array of 10 emements

--- src/__test__/ZooReducer.test.js
+++ src/__test__/ZooReducer.test.js
@@ -1,9 +1,13 @@
 import ZooReducer from "../redux/Zoo/Zoo";
-
+import { getAnimalsFromAPI } from "../redux/Zoo/Zoo";
 
 const initialState = [];
 describe('Tests for Redux in project', () => {
   it('Test if the state is initialy an empty array', () => {
     expect(ZooReducer(initialState, [])).toEqual([]);
+  });
+
+  it('Test if it returns a 10 items array', () => {
+    expect(ZooReducer(initialState, getAnimalsFromAPI(['Plains Zebra', 'Magpie Goose', 'Fairy Bluebird', 'Black Rhinoceros', 'Mosquitofish', 'Grey Crowned Crane', 'Alpaca', 'Dyeing Poison Dart Frog', 'Green Tree Monitor', 'Blue-Crowned Motmot']))).not.toEqual(['Plains Zebra', 'Magpie Goose', 'Fairy Bluebird', 'Black Rhinoceros', 'Mosquitofish', 'Grey Crowned Crane', 'Alpaca', 'Dyeing Poison Dart Frog', 'Green Tree Monitor', 'Blue-Crowned Motmot']);
   });
 });
