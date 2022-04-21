3e6c09d1db2a9ba201ac29a0b18836c1a22a2b17
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-04-21 08:44:10 +0300

    Update included more unit tests and fixed failing tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import renderer from 'react-test-renderer';
+import { cleanup } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import ZooReducer from './redux/Zoo/Zoo';
+
+const zoored = combineReducers({ animals: ZooReducer });
+const middleWares = [thunk, logger];
+
+const store = createStore(
+  zoored,
+  applyMiddleware(...middleWares),
+);
+
+afterEach(cleanup);
 import ZooContainer from './ZooContainer';
 
-test('renders learn react link', () => {
-  render(<ZooContainer />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test for the Zoo component', () => {
+  test('Check whether the zoo page renders correctly', () => {
+    const zoocont = renderer
+      .create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <ZooContainer />
+          </BrowserRouter>
+        </Provider>,
+      )
+      .toJSON();
+    expect(zoocont).toMatchSnapshot();
+  });
 });
